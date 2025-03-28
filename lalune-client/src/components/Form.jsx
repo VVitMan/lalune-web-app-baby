a9d009cc606fdfd9813973ab9b9c248e054c4c26
VitItthikon <103120749+VVitMan@users.noreply.github.com>
2025-03-28 18:40:20 +0700

    css Form.jsx update

--- lalune-client/src/components/Form.jsx
+++ lalune-client/src/components/Form.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Form() {
     return (
         <div className="flex justify-center items-center w-full  min-h-screen bg-base-50 px-5 py-5">
@@ -9,7 +11,7 @@ export default function Form() {
                 </div>
 
                 <div className="mx-auto w-full lg:w-1/2 md:p-10 py-5 md:py-0">
-                    <h1 className="text-center text-2xl sm:text-3xl font-semibold text-error">
+                    <h1 className="text-center text-2xl sm:text-3xl font-semibold text-[#f97675]">
                         Dispel
                     </h1>
                     <div className="w-full mt-5 sm:mt-8">
@@ -18,23 +20,23 @@ export default function Form() {
                                 <input
                                     type="text"
                                     placeholder="Enter Your First Name"
-                                    className="input input-bordered input-error w-full max-w-xs text-black placeholder:text-black/60"
+                                    className="input input-bordered w-full max-w-xs text-black placeholder:text-black/60"
                                 />
                                 <input
                                     type="text"
                                     placeholder="Enter Your Last Name"
-                                    className="input input-bordered input-error w-full max-w-xs text-black placeholder:text-black/60"
+                                    className="input input-bordered w-full max-w-xs text-black placeholder:text-black/60"
                                 />
                             </div>
                             
                             {/* Date */}
                             <input
                                 type="date"
-                                className="input input-bordered input-error w-full text-black/60"
+                                className="input input-bordered w-full text-black/60"
                             />
 
                             {/* Zodiac */}
-                            <select defaultValue="Pick a Zodiac" className="select select-error text-black/60">
+                            <select defaultValue="Pick a Zodiac" className="select select-bordered text-black/60">
                                 <option disabled={true}>Pick a Zodiac</option>
                                 <option>Rat</option>
                                 <option>Ox</option>
@@ -62,17 +64,20 @@ export default function Form() {
                                 </div>
                                 <h3 className="flex items-center whitespace-nowrap text-xs text-black">
                                     I agree to the
-                                    <span className="text-[#4A07DA]">&nbsp;Terms</span>
+                                    <span className="text-[#f97675]">&nbsp;Terms</span>
                                     &nbsp;and
-                                    <span className="text-[#4A07DA]">&nbsp;Privacy Policy</span>.
+                                    <span className="text-[#f97675]">&nbsp;Privacy Policy</span>.
                                 </h3>
                             </div>
 
                             {/* 2 Button, Sent and Cancel */}
                             <div className="flex flex-col md:flex-row gap-2 md:gap-4 justify-center items-center">
-                                <button className="btn btn-active btn-primary btn-block max-w-[200px]">
-                                    Cancel
-                                </button>
+                                <Link to={"/"}>
+                                    <button className="btn btn-active btn-primary btn-block max-w-[200px]">
+                                        Cancel
+                                    </button>
+                                </Link>
+
                                 <button className="btn btn-outline btn-primary btn-block max-w-[200px]">
                                     Sent
                                 </button>
